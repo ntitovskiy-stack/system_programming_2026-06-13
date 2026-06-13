a541141841740bba6bead12f585a208e8b24282b
ntitovskiy-stack <titonik301007@gmail.com>
2026-06-13 14:42:05 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+
+int main(int argc, char **argv)
+{
+  // отключение вывода потока ошибок
+	extern int opterr;
+	opterr = 0;
+	int is_correct = 1;
+	int opchar = 0;
+	int opindex = 0;
+	struct option opts[] = {
+    // опция с обязательным аргументом
+		{"query", required_argument, 0, 'q'},
+    // опции без аргументов
+		{"longinformationrequest", no_argument, 0, 'i'},
+		{"version", no_argument, 0, 'v'},
+		{0,0,0,0}
+	};
+  // перебор аргументов командной строки
+	while (-1 != (opchar = getopt_long(argc, argv, "q:iv", opts, &opindex))) {
+    // обработка конкретного аргумента
+		switch (opchar) {
+		case 'q':
+      // если опция q передана без аргумента
+			if (optarg == NULL || optarg[0] == '\0')
+				is_correct = 0;
+			break;
+		case 'i':
+			break;
+		case 'v':
+			break;
+		case '?':
+      // если указана несуществующая опция
+			is_correct = 0;
+			break;
+		default:
+			is_correct = 0;
+		}
+	}
+	printf("%c", is_correct ? '+' : '-');
+	return 0;
+}
